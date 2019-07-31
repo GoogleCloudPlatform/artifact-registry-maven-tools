a0092a243b81d65da9e3dc4ddb38ab51e9d6e71f
Lian Mo <molian@google.com>
2019-07-30 17:13:46 -0700

    make the gradle plugin insert credentials internally, and also make the plugin decrypt the buildartifacts scheme

--- build-artifacts-gradle-plugin/src/main/java/com/google/cloud/buildartifacts/gradle/plugin/BuildArtifactsGradlePlugin.java
+++ build-artifacts-gradle-plugin/src/main/java/com/google/cloud/buildartifacts/gradle/plugin/BuildArtifactsGradlePlugin.java
@@ -21,22 +21,81 @@ import com.google.auth.oauth2.AccessToken;
 import com.google.cloud.buildartifacts.wagon.DefaultCredentialProvider;
 import com.google.cloud.buildartifacts.wagon.CredentialProvider;
 import java.io.IOException;
+import java.net.URI;
+import org.gradle.api.artifacts.repositories.ArtifactRepository;
+import org.gradle.api.artifacts.repositories.PasswordCredentials;
+import org.gradle.api.credentials.Credentials;
+import org.gradle.api.internal.artifacts.repositories.DefaultMavenArtifactRepository;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.publish.PublishingExtension;
 
 public class BuildArtifactsGradlePlugin implements Plugin<Project> {
 
 	private CredentialProvider credentialProvider = new DefaultCredentialProvider();
 
-    //@Override
+    @Override
     public void apply(Project project) {
+        project.afterEvaluate(p -> {
+            project.getRepositories().all(this::configureBuildArtifactsRepositories);
+            final PublishingExtension publishingExtension = project.getExtensions().findByType(PublishingExtension.class);
+            if (publishingExtension != null) {
+                publishingExtension.getRepositories().all(this::configureBuildArtifactsRepositories);
+            }
+        });
+    }
+
+    public void configureBuildArtifactsRepositories(ArtifactRepository repo)
+    {
         try {
             GoogleCredentials credentials = (GoogleCredentials)credentialProvider.getCredential();
             AccessToken accessToken = credentials.getAccessToken();
             String token = accessToken.getTokenValue();
-            project.getExtensions().getExtraProperties().set("oauth2accesstoken", token);
+            
+            if (!(repo instanceof DefaultMavenArtifactRepository)) {
+                return;
+            }
+            final DefaultMavenArtifactRepository cbaRepo = (DefaultMavenArtifactRepository) repo;
+            final URI u = cbaRepo.getUrl(); 
+            if (u != null && u.getScheme() != null && u.getScheme().equals("buildartifacts")) {
+                String httpURL = u.toString().replaceFirst("buildartifacts://", "https://");
+                cbaRepo.setUrl(URI.create(httpURL));
+                BuildArtifactsPasswordCredentials crd = new BuildArtifactsPasswordCredentials("oauth2accesstoken", token);
+                cbaRepo.setConfiguredCredentials((Credentials)crd);
+            }
         } catch (IOException e) {
             System.out.println(e.toString());
         }
     }
+}
+
+class BuildArtifactsPasswordCredentials implements PasswordCredentials {
+    private String username;
+    private String password;
+
+    public BuildArtifactsPasswordCredentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setUsername(String username) {
+       this.username = username;
+    }
+
+    @Override
+    public void setPassword(String password) {
+       this.password = password;
+    }
+    
 }
