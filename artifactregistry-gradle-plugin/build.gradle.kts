0dc044e3f0f7d92480f7a1ad5ef2ebbaae052429
Tim Froehlich <froeht@google.com>
2025-06-27 14:55:40 -0500

    Migrate build.gradle to kotlin script
    (commit 2/2)

--- artifactregistry-gradle-plugin/build.gradle.kts
+++ artifactregistry-gradle-plugin/build.gradle.kts
@@ -1,29 +1,29 @@
 plugins {
-    id 'java-gradle-plugin'
+    `java-gradle-plugin`
     alias(libs.plugins.gradle.plugin.publish)
 }
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(8)
+        languageVersion.set(JavaLanguageVersion.of(8))
     }
 }
 
 dependencies {
-	implementation gradleApi()
-	implementation(libs.google.auth.library.oauth2.http)
+    implementation(gradleApi())
+    implementation(libs.google.auth.library.oauth2.http)
 }
 
 gradlePlugin {
-    website = 'https://github.com/GoogleCloudPlatform/artifact-registry-maven-tools'
-    vcsUrl = 'https://github.com/GoogleCloudPlatform/artifact-registry-maven-tools.git'
+    website.set("https://github.com/GoogleCloudPlatform/artifact-registry-maven-tools")
+    vcsUrl.set("https://github.com/GoogleCloudPlatform/artifact-registry-maven-tools.git")
     plugins {
-        artifactRegistryPlugin {
-            id = 'com.google.cloud.artifactregistry.gradle-plugin'
-            displayName = 'Artifact Registry Gradle Plugin'
-            description = 'A Gradle plugin used to connect to Artifact Registry Maven repositories.'
-            tags.set(['artifactregistry', 'gcp'])
-            implementationClass = 'com.google.cloud.artifactregistry.gradle.plugin.ArtifactRegistryGradlePlugin'
+        create("artifactRegistryPlugin") {
+            id = "com.google.cloud.artifactregistry.gradle-plugin"
+            displayName = "Artifact Registry Gradle Plugin"
+            description = "A Gradle plugin used to connect to Artifact Registry Maven repositories."
+            tags.set(listOf("artifactregistry", "gcp"))
+            implementationClass = "com.google.cloud.artifactregistry.gradle.plugin.ArtifactRegistryGradlePlugin"
         }
     }
 }
